0050fb285c43859e18bdd5d47bbacd3350cbf8a8
feranco <feranco@gmail.com>
2017-02-20 23:06:13 +0100

    update arrays exercises

--- source/arrays.cpp
+++ source/arrays.cpp
@@ -46,6 +46,70 @@ void testRotateSquareMatrix (int n) {
   dumpMatrix(m, n, n);
 }
 
+//if m(i,j) = v, set all elements of i-rpw and j-column to v 
+void transformMatrix (int** m, int rows, int cols, int v) {
+  bool first_row = false, first_col = false;
+	
+  for (int i = 0; i < cols; ++i) {
+    if (m[0][i] == v) {
+      first_row = true;
+      break;
+    } 
+  }
+	
+  for (int i = 0; i < rows; ++i) {
+    if (m[i][0] == v) {
+      first_col = true;
+      break;
+    } 
+  }
+	
+  for (int i = 1; i < cols; ++i) {
+    for (int j = 1; j < rows; ++j) {
+      if (m[i][j] == v) {
+	m[i][0] = v;
+	m[0][j] = v;
+      }
+    }
+  }
+	
+  for (int i = 0; i < cols; ++i) {}
+  if (m[0][i] == v) {
+    for (int j = 1; j < rows; ++j) m[j][i] = v;
+  } 
+}
+	
+for (int i = 0; i < rows; ++i) {
+  if (m[i][0] == v) {
+    for (int j = 1; j < rows; ++j) m[i][j] = v;
+  } 
+ }
+	
+if (first_row) {
+  for (int i = 0; i < cols; ++i) m[0][i] == v;
+ }
+	
+if (first_col) {
+  for (int i = 0; i < rows; ++i) m[i][0] == v;
+ }
+}
+
+void testTransformMatrix (int n) {
+  srand(time(0));
+  int** m;
+  m = new int*[n];
+  
+  for (int i = 0; i < n; ++i) {
+    m[i] = new int[n];
+    for (int j = 0; j < n; ++j) m[i][j] = rand()%256;
+  }
+
+  dumpMatrix(m, n, n);
+  std::cout << " -----  " << std::endl;
+  transformMatrix(m, n);
+  dumpMatrix(m, n, n);
+}
+
 void dumpMatrix (int** m, int rows, int cols) {
 
   for (int i = 0; i < cols; ++i) {
